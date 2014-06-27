b50924f3a9508d7945119b87285029e5e04b8f08
Sasha Kovar <sasha-git@arcocene.org>
2014-06-27 15:42:18 -0700

    Fix license from default Eclipse to GPL3

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject tatlin "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject tatlin "0.1.0"
+  :description "Tools for creating wireframe toolpaths for a 3D printer."
+  :url "https://github.com/abend/tatlin-clojure"
+  :license {:name "GPLv3"
+            :url "https://gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [slingshot "0.10.3"]
                  [quil "2.1.0"]
